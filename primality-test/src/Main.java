bd52c6b81385fa2370660a418dc31bf30ec254ac
Leonardo Lautenschlaeger <70241645+leolautens@users.noreply.github.com>
2022-12-30 01:44:22 -0300

    Java Primality Test

--- primality-test/src/Main.java
+++ primality-test/src/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BigInteger var;
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in))) {
             var = new BigInteger(bufferedReader.readLine());
