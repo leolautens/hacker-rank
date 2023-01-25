56dd56ea07d6cce4be3c049ae32d94cfc5c788b7
Leonardo Lautenschlaeger <70241645+leolautens@users.noreply.github.com>
2023-01-25 01:46:13 -0300

    Algorithms - Number Line Jumps

--- number-line-jumps/src/Main.java
+++ number-line-jumps/src/Main.java
@@ -24,11 +24,12 @@ class Result {
      */
 
     public static String kangaroo(int x1, int v1, int x2, int v2) {
-        // Write your code here
 
-        return null;
+        if (v2 >= v1 || x1 > x2) return "NO";
+        if (x1 == x2) return "YES";
+        return kangaroo(x1+v1, v1, x2+v2, v2);
+
     }
-
 }
 
 public class Main {
