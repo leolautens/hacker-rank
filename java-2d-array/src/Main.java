feea9473e531226179c766ddd291ff31a3b21e25
Leonardo Lautenschlaeger <70241645+leolautens@users.noreply.github.com>
2023-02-27 22:59:10 -0300

    Java 2D Array

--- java-2d-array/src/Main.java
+++ java-2d-array/src/Main.java
@@ -22,7 +22,23 @@ public class Main {
                 throw new RuntimeException(ex);
             }
         });
-
+        int sum=0,largest=Integer.MIN_VALUE;
+        for(int k=0;k<4;k++){
+            for(int j=0;j<4;j++){
+                sum=arr.get(k).get(j)+
+                        arr.get(k).get(j+1)+
+                        arr.get(k).get(j+2)+
+                        arr.get(k+1).get(j+1)+
+                        arr.get(k+2).get(j)+
+                        arr.get(k+2).get(j+1)+
+                        arr.get(k+2).get(j+2);
+                if(sum>largest){
+                    largest=sum;
+                }
+            }
+        }
+        System.out.println(largest);
         bufferedReader.close();
     }
 }
+
